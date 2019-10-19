3358d24f2ce57401f79dbd73896b0811ed96fda4
Asanga <asanga.nicholas@yahoo.com>
2019-10-19 18:44:10 +0200

    set  limit request

--- messages-api.js
+++ messages-api.js
@@ -6,7 +6,26 @@ const bodyParser = require('body-parser')
 
 const app = express()
 const port = 3000
-app.post('/messages', (req, res, next) => {
-    console.log('text', req.body)
+
+const maxMessages = 5
+let numOfRequests = 0
+const limitRequest = (req, res, next) => {
+  numOfRequests +=
+  console.log('num of req =', numOfRequests)
+  if(numOfRequests > maxMessages)
+   res.status(429).send({message: "Too Many Requests."})
+  else next()
+}
+
+
+
+app.use(bodyParser.json())
+app.post('/messages', limitRequest, (req, res, next) => {
+    console.log('text:', req.body)
+    if (req.body.text){
+    res.status(200).json({ message: "Message received loud and clear"})
+    } else{
+        res.status(400).json({message: "Bad Request"})
+    }
 })
 app.listen(port, () => console.log(`Messages api is listening on port ${port}!`))
