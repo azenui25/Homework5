f259ba1546c2ac1a02238d3aa23b4496c2f0d608
Asanga <asanga.nicholas@yahoo.com>
2019-10-19 16:08:57 +0200

    initiated request

--- messages-api.js
+++ messages-api.js
@@ -6,4 +6,7 @@ const bodyParser = require('body-parser')
 
 const app = express()
 const port = 3000
+app.post('/messages', (req, res, next) => {
+    console.log('text', req.body)
+})
 app.listen(port, () => console.log(`Messages api is listening on port ${port}!`))
